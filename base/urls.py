cdc3a90b47be35dcad12725f91a9be1b25c7844f
Anurag Vinodh <anurag.vinodh@gmail.com>
2022-09-28 15:53:17 -0400

    Completed Data Models for Rooms,Users,Topic,Messages

--- base/urls.py
+++ base/urls.py
@@ -1,10 +1,9 @@
 from re import M
 from django.contrib import admin
 from django.urls import path
-from django.http import HttpResponse
 from . import views
 
-urlpatterns = {
-    path('', views.home, name="home"),
-    path("room/<str:pk>/",views.room, name="room")
-}
+urlpatterns = [
+    path("", views.home, name="home"),
+    path("room/<str:pk>/", views.room, name="room"),
+]
--- base/views.py
+++ base/views.py
@@ -1,17 +1,24 @@
 from django.shortcuts import render
+from .models import Room
+
 # Create your views here.
 
-rooms = [
-    {'id':1, 'name':'Lets learn python!'},
-    {'id':2, 'name':'Django is Fun'},
-    {'id':3, 'name':'Anyone up for Node'}, 
-    {'id':4, 'name':'Benny\'s fun room ;)'}
-]
+# rooms = [
+#     {'id':1, 'name':'Lets learn python!'},
+#     {'id':2, 'name':'Django is Fun'},
+#     {'id':3, 'name':'Anyone up for Node'},
+#     {'id':4, 'name':'Benny\'s fun room ;)'}
+# ]
+
 
 def home(request):
-    context = {'rooms': rooms}
-    return render(request, 'base/home.html', context)
+    rooms = Room.objects.all()
+    print(rooms)
+    context = {"rooms": rooms}
+    return render(request, "base/home.html", context)
 
-def room(request,pk):
-    print(pk)
-    return render(request, 'base/room.html')
+
+def room(request, pk):
+    room = Room.objects.get(id=pk)
+    context = {"room": room}
+    return render(request, "base/room.html", context)
